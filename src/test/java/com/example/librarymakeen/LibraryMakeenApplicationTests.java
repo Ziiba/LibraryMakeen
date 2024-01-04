07e5cfe6dcfa8a5aa023d0a6419392c62f94a4b3
ziba saberi <zibasaberi400@gmail.com>
2024-01-04 12:11:10 +0330

    Initial commit

---
+++ src/test/java/com/example/librarymakeen/LibraryMakeenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarymakeen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMakeenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
